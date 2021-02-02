60a29b63303c2ac8b581332a5d26ef1a099d0b89
noihaus <sky2174@gmail.com>
2021-02-03 00:01:03 +0900

    * storyboard 제거, rootviewcontroller setting

--- idus/AppDelegate.swift
+++ idus/AppDelegate.swift
@@ -12,8 +12,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    let navigationController: UINavigationController = {
+        let navigationController = UINavigationController()
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.view.backgroundColor = .white
+        return navigationController
+    }()
+    
+    private var appCoordinator: AppCoordinator?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let coordinator = AppCoordinator(navigationController: navigationController)
+        coordinator.Start()
+        appCoordinator = coordinator
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 }
