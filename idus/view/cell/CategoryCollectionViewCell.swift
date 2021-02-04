ca3e8c0564e50b9033b6890c77ac37c4680688d2
noihaus <sky2174@gmail.com>
2021-02-04 12:34:10 +0900

    * font 변경

--- idus/view/cell/CategoryCollectionViewCell.swift
+++ idus/view/cell/CategoryCollectionViewCell.swift
@@ -12,21 +12,21 @@ class CategoryCollectionViewCell: UICollectionViewCell {
     lazy var categoryLabel: UILabel = {
         let label = UILabel()
         label.textColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-        label.font = .boldSystemFont(ofSize: 14)
+        label.font = .systemFont(ofSize: 14)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(categoryLabel)
-        let spacing: CGFloat = 8
-        categoryLabel.snp.makeConstraints { (make) in
-            make.edges.equalTo(self).inset(UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing))
-        }
         self.layer.cornerRadius = 10
         self.layer.masksToBounds = true
         self.layer.borderWidth = 1
         self.layer.borderColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1).cgColor
+        let spacing: CGFloat = 8
+        categoryLabel.snp.makeConstraints { (make) in
+            make.edges.equalTo(self).inset(UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing))
+        }
     }
     override func prepareForReuse() {
     }
